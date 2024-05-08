5947eb6a4fd0d6cdbc548a818fa085b88698910b
Y-Fujikawa <fujikawa0@gmail.com>
2024-05-08 21:53:25 +0900

    StepFunctionsのContextオブジェクトを使えることを確認
    Doc. https://docs.aws.amazon.com/ja_jp/step-functions/latest/dg/input-output-contextobject.html

--- modules/lambda/app/main.py
+++ modules/lambda/app/main.py
@@ -1,5 +1,8 @@
+import json
+
 def lambda_handler(event, context):
+    resultBody = { 'execution_name': event['execution_name'] }
     return {
         'statusCode': 200,
-        'body': 'Hello, World!'
+        'body': resultBody
     }
